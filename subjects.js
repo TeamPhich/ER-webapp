705e7b0c2e39778b7d53c7f0de3b07fb8d0e5a86
Quang0202 <quangqqhh36@gmail.com>
2019-11-18 00:48:42 +0700

    commit subject admin;

--- subjects.js
+++ subjects.js
@@ -8,15 +8,7 @@ $(document).ready(function() {
                     .addClass("p-0")
                     .append($('<input>')
                         .attr("type", "text")
-                        .attr("id","autofocus")
-                    )
-
-                )
-                .append($('<td>')
-                    .addClass("p-0")
-                    .append($('<input>')
-                        .attr("type", "text")
-
+                        .attr("id", "autofocus")
                     )
                 )
                 .append($('<td>')
@@ -24,26 +16,27 @@ $(document).ready(function() {
                     .append($('<input>')
                         .attr("type", "text")
                     )
+                )
+                .append($('<td>')
+                    .addClass("p-0")
+                    .append($('<input>')
+                        .attr("type", "text")
                     )
+                )
                 .append($('<td>')
                     .addClass("p-0")
                     .append($('<button>')
                         .addClass("btn btn-secondary m-auto")
                         .attr("type", "button")
                         .text("Import")
-                        .attr('id','show')
+                        .attr('id', 'show')
                     )
                 )
             )
 
         $('#autofocus').focus();
-    });
-    $("#delete").click(function () {
-        let x=document.querySelectorAll("tbody > tr");
-        $(x[x.length-1]).remove();
-    });
-
-    $("#show").click(function () {
-        $("#showModal").modal('show');
     })
-    })
+})
+    function modelAdd() {
+        $("#addModal").modal("show");
+    }
