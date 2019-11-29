90566407dfc2c430cb224299e53ab743a57af99c
imhson <phamhoangson99@gmail.com>
2019-11-30 02:17:35 +0700

    them cap nhat danh sach sinh vien va cac nut

--- admin/manageStudents.js
+++ admin/manageStudents.js
@@ -1,41 +1,67 @@
-//js delete row
+var editField;
 $(document).ready(function(){
+    //js delete row
     $(".fa-trash-alt").on("click", function() {
-            console.log($(this).parent().parent().toggle());
+            $(this).parent().parent().toggle();
             //update to server here
     });
-    $("#subTable").dataTable( {
+
+    //customize table
+    table = $("#subTable").DataTable( {
+        retrieve: true,
         "columnDefs": [
             { "orderable": false, "targets": 'no-sort' }
         ],
-        order: [[1, 'asc']]
+        order: [[1, 'asc']],
+        language: {
+            paginate: {
+                next: '<i class="fas fa-angle-double-right"></i>',
+                previous: '<i class="fas fa-angle-double-left"></i>'
+            }
+        },
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
+
     } );
+    //js print
+    $("#printButton").on("click",function(){
+        newWin= window.open("");
+        stylesheet = '//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css';
+        newWin.document.write('<html><head><title></title>' + '<link rel="stylesheet" href="' + stylesheet + '">' + '</head><body>' + $('#subTable')[0].outerHTML + '</body></html>');
+        newWin.print();
+        newWin.close();
+    })
+    //js add modal
+    $(".fa-edit").on("click",function () {
+        $("#editModal").modal("show");
+        editField=$(this).parent().parent().children();
+        $("#inputMSSV").val(editField[1].innerText);
+        $("#inputHoten").val(editField[2].innerText);
+        $("#inputLop").val(editField[3].innerText);
+        $("#inputNgaysinh").val(editField[4].innerText);
+        $("#inputTentaikhoan").val(editField[5].innerText);
+        $("#inputMatkhau").val(editField[6].innerText);
+    });
+
+    //js confirm and close modal
+    $("#confirmEditButton").on("click",function () {
+        editField[1].innerText=$("#inputMSSV")[0].value;
+        editField[2].innerText=$("#inputHoten")[0].value;
+        editField[3].innerText=$("#inputLop")[0].value;
+        editField[4].innerText=$("#inputNgaysinh")[0].value;
+        editField[5].innerText=$("#inputTentaikhoan")[0].value;
+        editField[6].innerText=$("#inputMatkhau")[0].value;
+        $("#modalEdit").modal("hide");
+        //update to server here
+    });
+    $("#importButton").on("click", function () {
+        $("#importModal").modal("show");
+    });
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
 });
 
-//no-sort col
 
-//js add modal
-var editField;
-function addModel(v) {
-    $("#modalEdit").modal("show");
-    editField=$(v).parent().parent().children();
-    $("#inputMSSV").val(editField[1].innerText);
-    $("#inputHoten").val(editField[2].innerText);
-    $("#inputLop").val(editField[3].innerText);
-    $("#inputNgaysinh").val(editField[4].innerText);
-    $("#inputTentaikhoan").val(editField[5].innerText);
-    $("#inputMatkhau").val(editField[6].innerText);
-}
-//js confirm and close modal
-function confirmEdit(v) {
-    editField[1].innerText=$("#inputMSSV")[0].value;
-    editField[2].innerText=$("#inputHoten")[0].value;
-    editField[3].innerText=$("#inputLop")[0].value;
-    editField[4].innerText=$("#inputNgaysinh")[0].value;
-    editField[5].innerText=$("#inputTentaikhoan")[0].value;
-    editField[6].innerText=$("#inputMatkhau")[0].value;
-    $("#modalEdit").modal("hide");
-    //update to server here
-}
 
 
